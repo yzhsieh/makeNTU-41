88137b2e827222e8e100953b9a64d0a10d582f77
yzhsieh <xyz102416@gmail.com>
2018-03-31 22:50:10 +0800

    10:49 basic web service

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+from PIL import Image
 from importlib import import_module
 import os
+import io
+import time
 from flask import Flask, render_template, Response, request
-
 # import camera driver
 if os.environ.get('CAMERA'):
     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
@@ -12,45 +14,77 @@ else:
 # Raspberry Pi camera module (requires picamera package)
 from camera_pi import Camera
 
-app = Flask(__name__)
+class webService():
+
+    def __init__(self):
+        flag = 0
+        self.app = Flask(__name__)
+        self.app.add_url_rule('/', 'index', self.index)
+        self.app.add_url_rule('/video_feed', 'video_feed', self.video_feed)
+        self.app.add_url_rule('/shot_and_video_feed', 'shot_and_video_feed', self.shot_and_video_feed)
+        self.app.add_url_rule('/test', 'test', self.test, methods=['GET', 'POST'])
+        self.app.add_url_rule('/status', 'status', self.status, methods=['GET', 'POST'])
+
+    def index(self):
+        """Video streaming home page."""
+        return render_template('index.html')
 
 
-@app.route('/')
-def index():
-    """Video streaming home page."""
-    return render_template('index.html')
+    def gen(self, camera):
+        global flag
+        """Video streaming generator function."""
+        while True:
+            time.sleep(0.05)
+            frame = camera.get_frame()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
-def gen(camera):
-    """Video streaming generator function."""
-    while True:
+    def shot_and_gen(self, camera):
+        global flag
+        """Video streaming generator function."""
         frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        image = Image.open(io.BytesIO(frame))
+        # image.thumbnail((1000,250))
+        image.save('test.jpg')
 
+        while True:
+            time.sleep(0.05)
+            frame = camera.get_frame()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),
+    def video_feed(self):
+        """Video streaming route. Put this in the src attribute of an img tag."""
+        return Response(self.gen(Camera()),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    def shot_and_video_feed(self):
+        return Response(self.shot_and_gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    if request.method == "GET":
-        print(">>>>>>>>>>>>in GET method")
-        return render_template('test.html')
-    else:
-        print(">>>>>>>>>>>>in POST method")
-        return render_template('test.html', btn=request.form['type'])
+
+    def test(self):
+        if request.method == "GET":
+            print(">>>>>>>>>>>>in GET method")
+            return render_template('test.html')
+        else:
+            print(">>>>>>>>>>>>in POST method")
+            return render_template('test.html', btn=request.form['type'])
 
 
 
-@app.route('/status', methods=['GET', 'POST'])
-def status():
-    return render_template("status.html", status="Good")
+    def status(self):
+        if request.method == "GET":
+            print(">>>>>>>>>>>>in GET method")    
+            return render_template("status.html", status="Good", shot=0)
+        else:
+            print(">>>>>>>>>>>>in POST method")
+            print(request.form)
+            return render_template("status.html", status="Good", shot=1)
+
 
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', threaded=True)
+    WS = webService()
+    WS.app.run(host='0.0.0.0', threaded=True)
     #app.run()
